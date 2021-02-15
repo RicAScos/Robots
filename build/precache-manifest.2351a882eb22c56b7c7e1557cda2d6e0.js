161f1a20c6e966c0d70628149569bea9944b3038
RicAScos <39667273+RicAScos@users.noreply.github.com>
2021-02-15 12:34:29 +0100

    Add files via upload

---
+++ build/precache-manifest.2351a882eb22c56b7c7e1557cda2d6e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6524db44e2a01c6585508249fb1540d8",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/Robots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2fa6a264d5ac533332ab",
+    "url": "/Robots/static/css/main.9b7873fe.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/Robots/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Robots/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa6a264d5ac533332ab",
+    "url": "/Robots/static/js/main.da49a10e.chunk.js"
+  },
+  {
+    "revision": "f756081b821c21819cc9",
+    "url": "/Robots/static/js/runtime-main.a416d013.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
